d549b151ee3bb337ed6bd4f7d69c251ff2c55781
Sonny-in-1999 <minsoo1022@gmail.com>
2024-08-05 14:08:58 +0900

    init project

---
+++ README.md
@@ -0,0 +1,17 @@
+# Title
+
+## Title
+
+### Content
+
+#### Content
+
+##### Hello!! 
+
+###### Hello more!!!!
+
+```java
+public static void main(String[] args) {
+    
+        }
+```
---
+++ src/main/java/kr/co/neighbor21/api/Neighbor21StudyApplication.java
@@ -0,0 +1,13 @@
+package kr.co.neighbor21.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neighbor21StudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Neighbor21StudyApplication.class, args);
+    }
+
+}
